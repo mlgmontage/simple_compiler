e1f79869df8a58c8a02a871a4d58359227d91cf6
mlgmontage <kettleofrassel1@gmail.com>
2020-05-02 01:12:51 -0700

    simple parser

--- main.js
+++ main.js
@@ -1,15 +1,68 @@
 console.log("compiler");
 
-const thread = "Hello my name is";
+const thread = "Paper 100";
+
+// const line = [1, 2, 3];
+// console.log(line.shift());
+// console.log(line.shift());
 
 // simple lexer
-const tokens = thread
-  .split(/\s+/)
-  .filter((t) => t.length > 0)
-  .map((token) => {
-    return isNaN(token)
-      ? { type: "word", value: token }
-      : { type: "number", value: token };
-  });
+function lexer(thread) {
+  const tokens = thread
+    .split(/\s+/)
+    .filter((t) => t.length > 0)
+    .map((token) => {
+      return isNaN(token)
+        ? { type: "word", value: token }
+        : { type: "number", value: token };
+    });
+
+  return tokens;
+}
+
+function parser(tokens) {
+  // Abstract syntax tree
+  let AST = {
+    type: "Drawing",
+    body: [],
+  };
+
+  // extract tokens
+  while (tokens.length > 0) {
+    let current_token = tokens.shift();
+
+    // analyze words
+    if (current_token.type === "word") {
+      switch (current_token.value) {
+        case "Paper":
+          let expression = {
+            type: "CallExpression",
+            name: "Paper",
+            arguments: [],
+          };
+
+          // if current token is CallExpression
+          let argument = tokens.shift();
+          if (argument.type === "number") {
+            expression.arguments.push({
+              type: "NumberLiteral",
+              value: argument.value,
+            });
+            AST.body.push(expression);
+          } else {
+            throw "Paper command must be followed by a number";
+          }
+          break;
+      }
+    }
+  }
+
+  return AST;
+}
+
+const tokens = lexer(thread);
+
+const parsed = parser(lexer(thread));
 
 console.table(tokens);
+console.log(parsed);
