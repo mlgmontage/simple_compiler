1c924272c7a73ddd58cafb7fcefb9dfe82f342fe
mlgmontage <kettleofrassel1@gmail.com>
2020-05-02 00:18:04 -0700

    simple lexer

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("compiler");
+
+const thread = "Hello, My name is Compiler";
+
+const tokens = thread.split(/\s+/).map((token) => {
+  return isNaN(token)
+    ? { type: "word", value: token }
+    : { type: "number", value: token };
+});
+
+console.log(tokens);
