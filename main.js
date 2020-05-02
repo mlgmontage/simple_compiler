bf8c4d9fdd73ff56275d6a3c5ea29a6cbeed4d9b
mlgmontage <kettleofrassel1@gmail.com>
2020-05-02 05:06:25 -0700

    lexer

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("compiler");
+
+const code = "3 + 4 + 5    ";
+
+function lexer(code) {
+  const tokens = code
+    .split(/\s+/)
+    .filter((n) => n.length > 0)
+    .map((n) => {
+      return isNaN(n)
+        ? { type: "operation", value: n }
+        : { type: "number", value: parseInt(n) };
+    });
+  return tokens;
+}
+
+console.log(code);
+console.table(lexer(code));
